813bccebfc28b465f0c3393d23a51b570e0d97cf
Trevor Hartman <trevorhartman@gmail.com>
2014-04-16 08:27:34 -0600

    lein-release plugin: preparing 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject static-cljs/lein-template "0.1.1-SNAPSHOT"
+(defproject static-cljs/lein-template "0.1.1"
   :description "Leiningen template to generate a static site generator with ClojureScript and Less CSS preconfigured"
   :url "https://github.com/devth/static-cljs"
   :lein-release {:deploy-via :clojars}
